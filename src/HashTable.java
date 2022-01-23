2cce91ac78bb1cc06b1e17c29d3ec7fd3fb2b156
apoorv-5369 <apoorv.11910589.lpu.in>
2022-01-23 22:53:17 +0530

    Initial commit

---
+++ src/HashTable.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class HashTable {
+    public static void main(String[] args){
+        Hashtable<Integer, Integer> ht = new Hashtable<>();
+        ht.put(123, 432);
+        ht.put(12, 2345);
+        ht.put(15, 5643);
+        ht.put(3, 321);
+        ht.remove(12);
+        System.out.println(ht);
+    }
+}
